dac53759cffe87b416075a4c397207140dce4623
samin <samincgs@gmail.com>
2023-09-22 21:37:22 -0400

    Game done:) people can now play the quiz game in their terminal

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,3 +1,15 @@
+question_answer_dict = {
+    "What does CPU stand for? " : "central processing unit", "What does GUI stand for? ": "graphical user interface",
+    "Who is the co-founder of Microsoft Corporation?" : "bill gates",
+    "Which programming language is often used for web development and is known for its client-side scripting capabilities?" : "javascript",
+    "What does HTML stand for in the context of web development?" : "hypertext transfer market language",
+    " Which computer operating system is developed by Apple Inc.?" : "ios",
+    "What does URL stand for in the context of web addresses?" : "universal resource locator"
+    
+    }
+
+counter = 0
+
 print("This is a quiz about computers!\n")
 
 playing = input("Do you want to play the game? (Please answer YES OR NO)\n")
@@ -7,8 +19,15 @@ if playing.upper() != "YES":
 
 print("Okay! Let's start right away!\n")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit".lower():
-    print("Correct Answer!")
-else:
-    print("Sorry! That's the Wrong Answer:(")
+for question, answer in question_answer_dict.items():
+    print(question)
+    
+    guess = input().lower()
+    
+    if guess == answer:
+        print("Correct Answer!\n")
+        counter += 1
+    else:
+        print("Wrong Answer:(\n")
+        
+print(f"\nYour Score is: {counter} / {len(question_answer_dict)}\n\nThank you for playing my quiz game:)")
