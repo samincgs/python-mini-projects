10f9cd9fe2d1925348f0c0adbfd6d25e95e7eaa9
samin <samincgs@gmail.com>
2023-09-22 21:19:18 -0400

    letting the user decide if they wanna play or quit

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,1 +1,7 @@
-print("This is a quiz about computers!")
+print("This is a quiz about computers!\n")
+
+playing = input("Do you want to play the game? (Please answer YES OR NO)\n")
+
+if playing.upper() != "YES":
+    quit()
+
