0bc28fb377f8bdbed8e02e28440f6557965582d9
samin <samincgs@gmail.com>
2023-09-22 21:23:18 -0400

    added a question

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -5,3 +5,10 @@ playing = input("Do you want to play the game? (Please answer YES OR NO)\n")
 if playing.upper() != "YES":
     quit()
 
+print("Okay! Let's start right away!\n")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit".lower():
+    print("Correct Answer!")
+else:
+    print("Sorry! That's the Wrong Answer:(")
